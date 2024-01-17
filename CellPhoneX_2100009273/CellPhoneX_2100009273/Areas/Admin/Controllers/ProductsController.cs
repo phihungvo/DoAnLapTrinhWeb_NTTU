cf9d5ba38c30979ded6bd62120c50542d56843b5
phihungvo <vophihung987@gmail.com>
2024-01-17 19:11:04 +0700

    second commnit

---
+++ CellPhoneX_2100009273/CellPhoneX_2100009273/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CellPhoneX_2100009273.Models;
+using PagedList;
+
+namespace CellPhoneX_2100009273.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Admin/Products
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Index(int? page)
+        {
+            int pageNumber = page ?? 1;
+            int pageSize = 8; 
+            var products = db.Products.OrderBy(o => o.ProPrice).Include(p => p.Category).ToPagedList(pageNumber, pageSize);
+            return View(products);
+        }
+
+        // GET: Admin/Products/Details/5
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Admin/Products/Create
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            ViewBag.CatId = new SelectList(db.Categories, "Id", "CatName");
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ProId,ProName,ProImage,ProPrice,CatId,isNew,bestSelling,stock_quantity")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CatId = new SelectList(db.Categories, "Id", "CatName", product.CatId);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Edit/5
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CatId = new SelectList(db.Categories, "Id", "CatName", product.CatId);
+            return View(product);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ProId,ProName,ProImage,ProPrice,CatId,isNew,bestSelling,stock_quantity")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CatId = new SelectList(db.Categories, "Id", "CatName", product.CatId);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Delete/5
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Product product = await db.Products.FindAsync(id);
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CellPhoneX_2100009273/CellPhoneX_2100009273/Controllers/ShoppingCartController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CellPhoneX_2100009273.Models;
+
+
+namespace CellPhoneX_2100009273.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private DBContext dBContext = new DBContext();
+        private string strCart = "Cart";
+
+        // GET: ShoppingCart
+  
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OrderNow(int? Id)
+        {
+            if (Id == null) 
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (Session[strCart] == null) 
+            {
+                List<Cart> ListCart = new List<Cart> 
+                {
+                  new Cart(dBContext.Products.Find(Id), 1)
+                };
+                Session[strCart] = ListCart; 
+            }
+            else
+            {
+                List<Cart> ListCart = (List<Cart>)Session[strCart];
+                int check = IsExistingCheck(Id);
+                if (check == -1)
+                    ListCart.Add(new Cart(dBContext.Products.Find(Id), 1));
+                else
+                    ListCart[check].Quantity++; 
+                Session[strCart] = ListCart;
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        private int IsExistingCheck(int? Id)
+        {
+            List<Cart> ListCart = (List<Cart>)Session[strCart];
+            for (int i = 0; i < ListCart.Count; i++)
+            {
+                if (ListCart[i].Product.ProId == Id)
+                    return i;
+            }
+            return -1;
+        }
+
+
+        public ActionResult RemoveItem(int? Id)
+        {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            int check = IsExistingCheck(Id);
+            List<Cart> ListCart = (List<Cart>)Session[strCart];
+            ListCart.RemoveAt(check);
+            if (ListCart.Count == 0)
+            {
+                Session[strCart] = null;
+            }
+            else
+            {
+                Session[strCart] = ListCart;
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public ActionResult UpdateCart(FormCollection field)
+        {
+            string[] quantities = field.GetValues("quantity");
+            List<Cart> ListCart = (List<Cart>)Session[strCart];
+            for (int i = 0; i < ListCart.Count; i++)
+            {
+                ListCart[i].Quantity = Convert.ToInt32(quantities[i]);
+            }
+            Session[strCart] = ListCart;
+            return RedirectToAction("Index");
+        }
+
+
+
+        public ActionResult ClearCart()
+        {
+            Session[strCart] = null;
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ProcessOrder(FormCollection field)
+        {
+            List<Cart> ListCart = (List<Cart>)Session[strCart];
+
+            var order = new CellPhoneX_2100009273.Models.Order()
+            {
+                CustomerName = field["cusName"],
+                CustomerPhone = field["cusPhone"],
+                CustomerEmail = field["cusEmail"],
+                CustomerAddress = field["cusAddress"],
+                OrderDate = DateTime.Now,
+                PaymentType = "Cash",
+                Status = "Processing"
+            };
+            dBContext.Orders.Add(order);
+            dBContext.SaveChanges();
+
+            foreach (Cart cart in ListCart)
+            {
+                OrderDetail orderDetail = new OrderDetail()
+                {
+                    OrderId = order.OrderId,
+                    ProductId = cart.Product.ProId,
+                    Quantity = Convert.ToInt32(cart.Quantity),
+                    Price = Convert.ToDouble(cart.Product.ProPrice)
+                };
+                dBContext.OrderDetails.Add(orderDetail);
+                dBContext.SaveChanges();
+            }
+            Session.Remove(strCart);
+            return View("OrderSuccess");
+        }
+    }
+}
